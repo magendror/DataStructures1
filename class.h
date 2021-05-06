3a6e207af3517b0cd5ab093287f8110a7904bc4d
magendror <magendror@gmail.com>
2021-05-06 22:33:10 +0300

    classes new

---
+++ class.h
@@ -0,0 +1,141 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+class CarModel
+{
+public:
+    int model_id;
+    bool sort_by_rate;
+    int type_id;
+    int num_of_sales;
+    int rate;
+    CarModel(int model_id,bool sort_by_rate,int type_id,int num_of_sales,int rate);
+    ~CarModel()=default;
+    bool operator>(CarModel& model2);
+};
+
+CarModel::CarModel(int model_id,bool sort_by_rate,int type_id,int num_of_sales,int rate):
+                                    type_id(type_id),model_id(model_id),sort_by_rate(sort_by_rate),
+                                    num_of_sales(num_of_sales),rate(rate)
+{
+}
+
+CarModel::~CarModel()
+{
+}
+bool CarModel::operator>(CarModel& model2){
+    assert(sort_by_rate==model2.sort_by_rate);
+    //check by rate//
+    if(sort_by_rate==true){
+        if(rate>model2.rate){
+            return true;
+        }
+        else if(rate<model2.rate){
+            return false;
+        }
+        else{
+            if(type_id>model2.type_id){
+                return true;
+            }
+            else if(type_id<model2.type_id){
+                return false;
+            }
+            else{
+                if(model_id>model2.model_id){
+                    return true;
+                }
+            else if(model_id<model2.model_id){
+                return false;
+                }
+            }
+        }
+    }
+    //check by rate//
+    else if(sort_by_rate==false){
+        if(num_of_sales>model2.num_of_sales){
+            return true;
+        }
+        else if(num_of_sales<model2.num_of_sales){
+            return false;
+        }
+        else{
+            if(type_id>model2.type_id){
+                return false;
+            }
+            else if(type_id<model2.type_id){
+                return true;
+            }
+            else{
+                if(model_id>model2.model_id){
+                    return false;
+                }
+            else if(model_id<model2.model_id){
+                return true;
+                }
+            }
+        }
+    }
+}
+
+class CarType
+{
+public:
+    int type_id;
+    int num_of_models;
+    int best_seller;
+    int best_seller_sales;
+    int* statistics;
+    CarModel** pointers;
+    CarModel* zero_list;
+    CarType(int type_id,int num_of_models);
+    ~CarType();
+    bool operator>(CarType& model2);
+};
+
+CarType::CarType(int type_id,int num_of_models):
+        type_id(type_id),num_of_models(num_of_models),pointers(new CarModel*[num_of_models]),
+                statistics(new int[2*num_of_models])
+                {}
+
+CarType::~CarType()
+{
+    delete[] pointers;
+    delete[] statistics;
+}
+bool CarType::operator>(CarType& type2){
+    assert(type_id==type2.type_id);
+    if(type_id>type2.type_id){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+class Dealership
+{
+public:
+    CarType* type_tree;
+    CarType* new_tree;
+    CarModel* rate_tree;
+    CarType* sales_tree;
+    CarModel* WorstRate;
+    CarType* WorstRateNew;
+    CarModel* WorstSecondRate;
+    CarType* WorstSecondRateNew;
+    int NumOfModels;
+    CarModel* MaxSales;
+    Dealership();
+    ~Dealership();
+};
+
+Dealership::Dealership():type_tree(NULL),new_tree(NULL),rate_tree(NULL),sales_tree(NULL), WorstRate(NULL),
+                        WorstRateNew(NULL),WorstSecondRate(NULL),WorstSecondRateNew(NULL),NumOfModels(0),
+                        MaxSales(NULL)
+{
+}
+
+Dealership::~Dealership()
+{
+}
